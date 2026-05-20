7961a74711b5d0145804748034ffc996bb799029
William Alexander <williamax2005@gmail.com>
2026-05-21 05:14:59 +0700

    refactor loss

--- nn/loss.py
+++ nn/loss.py
@@ -1,31 +1,23 @@
 import numpy as np
 
 
+class Loss:
+    def __init__(self, loss_value: int, grad: int):
+        self.loss_value = loss_value
+        self.grad = grad
+
+    def __str__(self):
+        return str(self.loss_value)
+    
+    def __float__(self):
+        return self.loss_value
+    
+    def backward(self):
+        return self.grad
+
+
 # =============== MEAN SQUARED ERROR ===============
 def mse(y_pred, y_true):
-    """
-    Calculate the Mean Squared Error
-    MSE = ||y_true - y_pred||^2
-
-
-    --- Input ---
-    y_pred: predicted value
-    y_true: target value
-
-    --- Output --- 
-    Returns: loss
-    loss: MSE of y_pred
-    """
-    if y_pred.shape != y_true.shape:
-        msg = f"\Shape mismatched! \
-            y_pred 's shape: {y_pred.shape}, y_true's shape: {y_true.shape}"
-        raise ValueError(msg)
-
-    loss = 0.5 * ((y_true - y_pred) ** 2)
-    return loss
-
-
-def mse_with_grad(y_pred, y_true):
     """
     Calculate the Mean Squared Error and gradient of the MSE loss
     MSE = ||y_true - y_pred||^2
@@ -37,39 +29,21 @@ def mse_with_grad(y_pred, y_true):
     y_true: target value
 
     --- Output --- 
-    Returns: loss, grad_out
-    loss: MSE of y_pred
-    grad_out: gradient of the loss with respect to y_pred
-    """
-    loss = mse(y_pred, y_true)
-    grad_out = y_true - y_pred
-    return loss, grad_out
-
-
-# =============== BINARY CROSS ENTROPY ===============
-
-def binary_cross_entropy(y_pred, y_true):
-    """
-    Calculate the Binary Cross Entropy
-    BCE = -1/N * sum{y_i * log(p_i) + ((1-y_i) * log (1-p_i))}
-
-    --- Input ---
-    y_pred: target value
-    y_true: target value
-
-    --- Output ---
-    Returns: loss
-    loss: BCE of y_pred
+    Returns Loss(loss_value, grad_out)
     """
     if y_pred.shape != y_true.shape:
         msg = f"\Shape mismatched! \
             y_pred 's shape: {y_pred.shape}, y_true's shape: {y_true.shape}"
         raise ValueError(msg)
 
-    bce = -np.mean(y_true * np.log(y_pred) + (1 - y_true) * np.log(1 - y_pred))
-    return bce
+    loss_value = np.mean(((y_pred - y_true) ** 2), axis=0)
+    grad_out = 2/y_pred.shape[0] * (y_pred - y_true)
 
-def binary_cross_entropy_with_grad(y_pred, y_true):
+    return Loss(loss_value, grad_out)
+
+
+# =============== BINARY CROSS ENTROPY ===============
+def bce(y_pred, y_true):
     """
     Calculate the Binary Cross Entropy and gradient
     BCE = -1/N * sum{y_i * log(p_i) + ((1-y_i) * log (1-p_i))}
@@ -79,11 +53,15 @@ def binary_cross_entropy_with_grad(y_pred, y_true):
     y_true: target value
 
     --- Output ---
-    Returns: loss, grad_out
-    loss: BCE of y_pred
-    grad_out: Gradient of BCE with respect to y_pred
+    Returns Loss(loss_value, grad_out)
     """
-    loss = binary_cross_entropy(y_pred, y_true)
+    if y_pred.shape != y_true.shape:
+        msg = f"\Shape mismatched! \
+            y_pred 's shape: {y_pred.shape}, y_true's shape: {y_true.shape}"
+        raise ValueError(msg)
+
+    loss_value = -np.mean(y_true * np.log(y_pred) + (1 - y_true) * np.log(1 - y_pred))
+
     grad_out = np.mean((-y_true/y_pred) + ((1-y_true)/(1-y_pred)))
-    return loss, grad_out
+    return Loss(loss_value, grad_out)
 
