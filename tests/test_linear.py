dee68bf0cc40ab49aff03301226482010068c590
William Alexander <williamax2005@gmail.com>
2026-05-23 07:02:47 +0700

    add test for refactoring the dimension (batch_size for row)

--- tests/test_linear.py
+++ tests/test_linear.py
@@ -11,20 +11,10 @@ from nn.loss import *
 def test_forward_shape(input_size, output_size, batch_size):
     linear = Linear(input_size, output_size)
 
-    x = np.random.randn(input_size, batch_size)
+    x = np.random.randn(batch_size, input_size)
     y = linear.forward(x)
     
-    assert y.shape == (output_size, batch_size)
-
-@pytest.mark.parametrize("input_size",  range(3, 30, 6))   
-@pytest.mark.parametrize("output_size", range(1, 100, 10)) 
-def test_forward_shape_with_x_ndim_1(input_size, output_size):
-    linear = Linear(input_size, output_size)
-
-    x = np.random.randn(input_size)
-    y = linear.forward(x)
-
-    assert y.shape == (output_size, 1)
+    assert y.shape == (batch_size, output_size)
 
 
 @pytest.mark.parametrize("input_size", range(3, 12, 3))
@@ -32,16 +22,21 @@ def test_forward_shape_with_x_ndim_1(input_size, output_size):
 @pytest.mark.parametrize("batch_size", range(1, 100, 10))
 def test_backward_shape(input_size, output_size, batch_size):
     linear = Linear(input_size, output_size)
-    x = np.random.randn(input_size, batch_size)
+    x = np.random.randn(batch_size, input_size)
     y_pred = linear.forward(x)
 
     y_true = np.random.randn(output_size)
 
     loss = mse(y_pred, y_true)
-    assert loss.backward().shape == (output_size, batch_size)
+    assert loss.backward().shape == (batch_size, output_size)
 
     grad = linear.backward(loss.backward())
-    assert grad.shape == (input_size, batch_size)
+
+    assert linear.dw.shape == (output_size, input_size)
+    assert linear.db.shape == (output_size, 1)
+
+
+    assert grad.shape == (batch_size, input_size)
     
 
 
